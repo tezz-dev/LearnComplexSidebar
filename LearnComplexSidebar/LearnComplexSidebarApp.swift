225b49e469de8d9e91642932d76973c4aa6d7b63
tezz-io <matblockgreninja@gmail.com>
2020-11-10 20:03:26 +0530

    Initial Commit

---
+++ LearnComplexSidebar/LearnComplexSidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnComplexSidebarApp.swift
+//  LearnComplexSidebar
+//
+//  Created by Tejas M R on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LearnComplexSidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
